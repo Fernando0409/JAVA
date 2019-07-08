f5c784db59423e04e5fa536bbfcf8e33660c9efb
Luis Fernando Martinez Silva <fernando04_software@outlook.com>
2019-07-08 02:15:10 -0500

    New methods
    I added new methods that help to better the program's efficiency

--- Primeros_Pasos/aplicacionesgraficas/Marcos.java
+++ Primeros_Pasos/aplicacionesgraficas/Marcos.java
@@ -11,10 +11,21 @@ public class Marcos{
 }
 
 class MyFrame extends JFrame{
-    
-    public MyFrame(String title){
+
+    public MyFrame(String title){   
         super(title);   // Call to constructor of class JFrame
-        setSize(500, 300); // Width, Height 
+        
+        /*setSize(500, 300); // Width, Height
+        setLocation(400,300);*/
+
+        setBounds(400, 300, 500, 300);
+        setResizable(false); 
+        
         setVisible(true);
     }
 }
+
+/*
+    Note:
+    PC's resolution: 1366, 768 (16:9)
+*/
