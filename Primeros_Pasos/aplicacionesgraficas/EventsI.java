9ba2aff3d3156c3023053d1355f55eb7c967d41e
Luis Fernando Martinez Silva <fernando04_software@outlook.com>
2019-07-12 03:52:13 -0500

    Update Events
    Creation of 3 buttons that changes the panel's color (yellow, red, blue) doing 'click'

--- Primeros_Pasos/aplicacionesgraficas/EventsI.java
+++ Primeros_Pasos/aplicacionesgraficas/EventsI.java
@@ -7,30 +7,45 @@ import javax.swing.JPanel;
 import javax.swing.JButton;
 
 public class EventsI{
+    public static void main(String [] args){	
+		Frame frame = new Frame("Events I.");	// Create the window
+		PanelEventsI panelEventsI = new PanelEventsI();	// Create the panel that contain my components
 
-        public static void main(String [] args){	
-                Frame frame = new Frame("Events I.");	// Create the window
-                PanelEventsI panelEventsI = new PanelEventsI();	// Create the panel that contain my components
-
-                frame.add(panelEventsI);	// Add the panel to my window
+		frame.add(panelEventsI);	// Add the panel to my window
 		frame.setVisible(true);		// To see the window
-                frame.setDefaultCloseOperation(3); // To close the window
-        }
+        frame.setDefaultCloseOperation(3); // To close the window
+    }
 }
 
 class PanelEventsI extends JPanel implements ActionListener{
 
-	JButton button = new JButton("Blue Button");
+	JButton blueButton = new JButton("Blue Button");
+	JButton yellowButton = new JButton("Yellow Button");
+	JButton redButton = new JButton("Red Button");
 
 	public PanelEventsI(){
-		add(button); // Show button in the panel
-		button.addActionListener(this); // Listener the type 'click'
+		// Show the buttons in the panel
+		add(blueButton); 
+		add(redButton);
+		add(yellowButton);
+
+		// No se que poner pero establecemos el tipo de evento, quien lo hace y quien lo recibira/sufrira los cambios
+		// objectSource.typeEvent(Listener)
+		blueButton.addActionListener(this);
+		yellowButton.addActionListener(this);
+		redButton.addActionListener(this);
 	}
 
-	// Action that realized the button, action listener's method 
+	// Action that realized the blueButton, action listener's method 
 	public void actionPerformed(ActionEvent e){
-		setBackground(Color.BLUE);
+		// Gets the source of event & changes the color
+		Object sourceEvent = e.getSource();
+
+		if(sourceEvent == blueButton)
+			setBackground(Color.BLUE);
+		else if(sourceEvent == redButton)
+			setBackground(Color.RED);
+		else
+			setBackground(Color.YELLOW);
 	}
 }
-
-// Create a button, this will change the backkground
